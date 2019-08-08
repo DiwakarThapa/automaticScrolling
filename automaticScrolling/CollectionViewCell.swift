6fdac766bd76f086a13228acdba8e9f15743f719
diwakar thapa <diwakarthapa@diwakars-Mac-mini.local>
2019-08-08 17:50:33 +0545

    startted

---
+++ automaticScrolling/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  automaticScrolling
+//
+//  Created by diwakar thapa on 8/8/19.
+//  Copyright © 2019 ekbana. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
